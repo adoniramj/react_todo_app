68bd1398140617479f3cbffc70336f1b912f273d
adoniramj <adoniramjvargas@gmail.com>
2019-12-14 16:54:51 -0500

    Add to the heading the amount of items that are due.

--- src/App.js
+++ src/App.js
@@ -9,9 +9,10 @@ export default class App extends Component {
        todoItems: [
          {action: 'Buy paper', done: false},
          {action: 'Change oil', done: false},
-         {action: 'Buy a car', done: true}
+         {action: 'Buy a car', done: false}
        ],
-       newItem: ''
+       newItem: '',
+       itemsDue: ''
     }
   }
 
@@ -25,14 +26,27 @@ export default class App extends Component {
         todoItems: [...this.state.todoItems,
                    { action: this.state.newItem, done: false}],
         newItem: ''
-      })
+      }, () => this.itemReport()) //setState(updater[, callback]). setState is async.
     }
   }
+
+  itemReport = () => {
+    let itemsDue = this.state.todoItems.filter(item => !item.done).length
+    if (itemsDue === 1){
+      this.setState({ itemsDue: `${itemsDue} item is due`})
+    } else if (itemsDue > 1) {
+      this.setState({ itemsDue: `${itemsDue} items are due`})
+    } else {
+      this.setState({ itemsDue: ''})
+    }
+  }
+
   render() {
     return (
       <div>
         <h4 className='bg-primary text-white text-center p-2'>
         {this.state.userName}'s To Do List
+        ({ this.state.itemsDue })
         </h4>
         <div className='container-fluid'>
           <div className='my-1'>
@@ -46,6 +60,10 @@ export default class App extends Component {
         </div>
       </div>
     )
+  }
+
+  componentDidMount(){
+    this.itemReport()
   }
 }
 
