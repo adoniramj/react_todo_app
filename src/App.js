c2d32b5af582fa16801ab9d3b6a23bc3dc280184
adoniramj <adoniramjvargas@gmail.com>
2019-12-14 09:43:56 -0500

    Modification to App.js - updateNewItem function - addNewItem function

--- src/App.js
+++ src/App.js
@@ -1,26 +1,51 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class App extends Component {
+
+  constructor(props) {
+    super(props)
+    this.state = {
+       userName: 'Adoniram',
+       todoItems: [
+         {action: 'Buy paper', done: false},
+         {action: 'Change oil', done: false},
+         {action: 'Buy a car', done: true}
+       ],
+       newItem: ''
+    }
+  }
+
+  updateNewItem = event => {
+    this.setState({ newItem: event.target.value})
+  }
+
+  addNewItem = () => {
+    if(!this.state.todoItems.find(item => item.action === this.state.newItem)) {
+      this.setState({
+        todoItems: [...this.state.todoItems,
+                   { action: this.state.newItem, done: false}],
+        newItem: ''
+      })
+    }
+  }
+  render() {
+    return (
+      <div>
+        <h4 className='bg-primary text-white text-center p-2'>
+        {this.state.userName}'s To Do List
+        </h4>
+        <div className='container-fluid'>
+          <div className='my-1'>
+              <input className='form-control'
+                     value={ this.state.newItem }
+                     onChange={ this.updateNewItem } 
+              />
+              <button className='btn btn-primary mt-1'
+                      onClick={ this.addNewItem }>Add New Item</button>
+          </div>
+        </div>
+      </div>
+    )
+  }
 }
 
-export default App;
