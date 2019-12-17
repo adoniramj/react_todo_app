39b4a871feadfc91b2ec0179e4e37009cb564416
adoniramj <adoniramjvargas@gmail.com>
2019-12-17 13:27:27 -0500

    Add child component to App.js to will new items. The child component is named ItemInsertion.js

--- src/App.js
+++ src/App.js
@@ -23,6 +23,13 @@ export default class App extends Component {
     { todoItems: this.state.todoItems.map(
       item => item.action === todo.action ? {...item, done: !item.done} : item)
     })
+    //createNewItem is set to ItemInsertion as props. 
+    //This is required to be defined in App.js because it needs to access todoItems.
+    createNewItem = (task) => {
+      if (!this.state.todoItems.find(item => item.action === task)){
+          this.setState({todoItems: [{action: task, done: false}, ...this.state.todoItems]})
+      }
+    }
 
   //Evert time there is a change in state the render function is executed.
   render() {
@@ -30,7 +37,8 @@ export default class App extends Component {
       <div>
         {/* See the syntax for importing Header and Itemstable. They are different. */}
         <Header name={this.state.userName} tasks={this.state.todoItems}/>
-        <ItemInsertion />
+        <ItemInsertion callback={ this.createNewItem }/>
+        {/* Passing the createNewItem function to ItemInsertion component. Notice that inside createNewItem this.state.todoItems "tags" along with the function.*/}
         <ItemsTable tasks={this.state.todoItems} callback={this.toggleDone}/>
       </div>
     )
--- src/ItemInsertion.js
+++ src/ItemInsertion.js
@@ -11,6 +11,11 @@ export class ItemInsertion extends Component {
   ItemChange = (event) => {
     this.setState({ newItem: event.target.value})
   }
+
+  InsertNewItem = () => {
+    this.props.callback(this.state.newItem)
+    this.setState({ newItem: ''})
+  }
   render() {
     return (
       <div className='my-1'>
@@ -18,6 +23,9 @@ export class ItemInsertion extends Component {
             className='form-control'
             value={ this.state.newItem }
             onChange={ this.ItemChange } />
+          <button 
+            className='btn btn-primary mt-1'
+            onClick={ this.InsertNewItem }>Add</button>
       </div>
     )
   }
